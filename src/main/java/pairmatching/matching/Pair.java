de205c145caf40d60c83538b507853af8dddd673
Jang <sdsd090811@gmail.com>
2024-12-11 18:10:14 +0900

    feat: 페어 기능 추가

--- src/main/java/pairmatching/matching/Pair.java
+++ src/main/java/pairmatching/matching/Pair.java
@@ -1,4 +1,23 @@
 package pairmatching.matching;
 
+import java.util.List;
+
 public class Pair {
+    private List<String> crewNames;
+
+    public Pair(List<String> names) {
+        if(names.size() > 3 || names.size() < 2)
+            throw new IllegalArgumentException("페어는 2~3명이 필수입니다.");
+        crewNames = names;
+    }
+
+    public int countDuplicatedCrew(Pair pair) {
+        return (int) crewNames.stream()
+                .filter(pair.getCrewNames()::contains)
+                .count();
+    }
+
+    protected List<String> getCrewNames() {
+        return crewNames;
+    }
 }
---
+++ src/test/java/pairmatching/matching/PairTest.java
@@ -0,0 +1,37 @@
+package pairmatching.matching;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PairTest {
+
+    @Test
+    void 페어는_3명을_넘어가면_예외발생() {
+        assertThrows(IllegalArgumentException.class, () -> new Pair(List.of("AAA", "BBB", "CCC", "DDD")));
+    }
+
+    @Test
+    void 페어는_2명보다_작으면_예외발생() {
+        assertThrows(IllegalArgumentException.class, () -> new Pair(List.of("AAA")));
+    }
+
+    @Test
+    void 특정_크루원이_얼마나_포함됐는지_확인가능() {
+        assertEquals(pair(List.of("AAA", "BBB"))
+                .countDuplicatedCrew(pair(List.of("DDD", "EEE"))), 0);
+        assertEquals(pair(List.of("AAA", "BBB"))
+                .countDuplicatedCrew(pair(List.of("BBB", "CCC"))), 1);
+        assertEquals(pair(List.of("AAA", "BBB"))
+                .countDuplicatedCrew(pair(List.of("BBB", "AAA"))), 2);
+        assertEquals(pair(List.of("AAA", "BBB"))
+                .countDuplicatedCrew(pair(List.of("AAA", "BBB", "CCC"))), 2);
+    }
+
+    private static Pair pair(List<String> names) {
+        return new Pair(names);
+    }
+
+}
